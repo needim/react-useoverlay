07a54c8831475045cc14b5a49ec07023844800b4
Nedim Arabacı <nedim.arabaci@limitforex.com>
2022-06-26 13:48:47 +0300

    docs update

--- docs/_next/static/a4linZDOjNyIuUd5WhQ8x/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fapi-AttachedOverlay","\u002Fapi-UnAttachedOverlay","\u002Fcomponent-tooltip","\u002Fhow-to-contribute","\u002Finstallation","\u002Fsponsors","\u002Fapi-useOverlay","\u002F"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
